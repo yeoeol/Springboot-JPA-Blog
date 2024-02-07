f90ee3866f2172f57b5e5adef5a0b3f90bff0cef
yeoeol <gnsl3528@naver.com>
2024-02-07 18:10:20 +0900

    회원가입 완료

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -23,7 +23,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라감
     private int id; // 시퀀스, auto_increment
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, unique = true)
     private String username;    // 아이디
 
     @Column(nullable = false, length = 100)
