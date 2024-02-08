a2ade5dd66afd8b0bb489676b8562a72902f156b
yeoeol <gnsl3528@naver.com>
2024-02-08 17:01:12 +0900

    글쓰기, 글목록보기, 글목록페이징, 글삭제하기, 글수정하기 완료

---
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -0,0 +1,39 @@
+package com.cos.blog.controller.api;
+
+import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.Board;
+import com.cos.blog.model.User;
+import com.cos.blog.service.BoardService;
+import com.cos.blog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class BoardApiController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/board")
+    public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
+        boardService.write(board, principal.getUser());
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @DeleteMapping("/board/{id}")
+    public ResponseDto<Integer> deleteById(@PathVariable int id) {
+        boardService.delete(id);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @PutMapping("/board/{id}")
+    public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board) {
+        boardService.update(id, board);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+
+    }
+}
