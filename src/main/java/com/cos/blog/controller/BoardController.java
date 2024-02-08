ee89fe15a054384fce8236117c7489046059f6d8
yeoeol <gnsl3528@naver.com>
2024-02-08 17:01:12 +0900

    글쓰기, 글목록보기, 글목록페이징, 글삭제하기, 글수정하기 완료

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,18 +1,43 @@
 package com.cos.blog.controller;
 
-import com.cos.blog.config.auth.PrincipalDetail;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.cos.blog.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping("/")
-    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
-        // /WEB-INF/views/index.jsp
-        System.out.println("로그인 사용자 아이디 : " + principal.getUsername());
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("boards", boardService.list(pageable));
         return "index";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, Model model) {
+        System.out.println("BoardController.detail");
+        model.addAttribute("board", boardService.detail(id));
+        return "board/detail";
+    }
+
+    @GetMapping("/board/{id}/update")
+    public String updateForm(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.detail(id));
+        return "board/updateForm";
+    }
+
+    // USER 권한이 필요
+    @GetMapping("/board/save")
+    public String saveForm() {
+        return "board/saveForm";
     }
 }
---
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -0,0 +1,55 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.Board;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+//스프링이 컴포넌트 스캔을 통해서 Bean에 등록해줌. IoC를 해줌
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+
+    public final BoardRepository boardRepository;
+
+    public void write(Board board, User user) {    //title, content
+        board.setCount(0);
+        board.setUser(user);
+        boardRepository.save(board);
+    }
+
+
+    @Transactional(readOnly = true)
+    public Page<Board> list(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Board detail(int id) {
+        return boardRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.")
+                );
+    }
+
+    public void delete(int id) {
+        System.out.println("글 삭제하기: " + id);
+        boardRepository.deleteById(id);
+    }
+
+    public void update(int id, Board requestBoard) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("글 찾기 실패: 아이디를 찾을 수 없습니다.")
+                );  // 영속화 완료
+        board.setTitle(requestBoard.getTitle());
+        board.setContent(requestBoard.getContent());
+        // 해당 함수 종료 시(service가 종료될 때) 트랜잭션이 종료됨 -> 더티체킹 - 자동 업데이트, db flush
+    }
+}
