70c3c436114fff4ed6711468c77efb55959e7373
yeoeol <gnsl3528@naver.com>
2024-02-13 15:29:39 +0900

    회원가입 오류, 게시글 삭제 오류 수정 및 댓글 삭제 구현

--- src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -1,9 +1,9 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.dto.ReplySaveRequestDto;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.Board;
-import com.cos.blog.model.Reply;
 import com.cos.blog.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -35,10 +35,16 @@ public class BoardApiController {
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 
+    // 데이터 받을 때 컨트롤러에서 dto를 만들어서 받는게 좋음
     @PostMapping("/board/{boardId}/reply")
-    public ResponseDto<Integer> replySave(@PathVariable int boardId, @RequestBody Reply reply, @AuthenticationPrincipal PrincipalDetail principal) {
-
-        boardService.commentWrite(principal.getUser(), boardId, reply);
+    public ResponseDto<Integer> replySave(@RequestBody ReplySaveRequestDto replySaveRequestDto) {
+        boardService.replyWrite(replySaveRequestDto);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @DeleteMapping("/board/{boardId}/reply/{replyId}")
+    public ResponseDto<Integer> replyDelete(@PathVariable int replyId) {
+        boardService.replyDelete(replyId);
+        return new ResponseDto<>(HttpStatus.OK.value(), 1);
     }
 }
